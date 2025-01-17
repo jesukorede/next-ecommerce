60ee44fc555d7a256fb38736df713dda0c1b2720
unknown <benjaminjesukorede@gmail.com>
2025-01-17 15:05:36 +0100

    Your commit message here

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React, { use } from 'react'
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+const SearchBar = () => {
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
+            
+    if(name) {
+
+    }
+  };
+  return (
+    <form className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1" onSubmit={handleSearch}>
+        <input type="text" name="name" placeholder='Search' className='flex-1 bg-transparent outline-none'/>
+        <button className='cursor-pointer'>
+            <Image src="/search.png" alt="" width={16} height={16}/>
+        </button>
+    </form>
+  )
+}
+
+export default SearchBar
